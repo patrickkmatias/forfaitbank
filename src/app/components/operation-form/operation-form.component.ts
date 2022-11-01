e7f55e08876c5b70b663c0e38989d33f9cbc1fff
Patrick M <patrickmatias.silva@hotmail.com>
2022-11-01 15:15:22 +0000

    content created and stylized operation-form component

--- src/app/components/operation-form/operation-form.component.ts
+++ src/app/components/operation-form/operation-form.component.ts
@@ -6,10 +6,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./operation-form.component.css']
 })
 export class OperationFormComponent implements OnInit {
-
   constructor() { }
 
   ngOnInit(): void {
+    this.initOperationForm();
+  }
+
+  initOperationForm() {
+  }
+
+  toggleButton(note: number) {
+
+    // note == 10 ? this.note10 = !this.note10 : undefined; 
+    // note == 50 ? this.note50 = !this.note50 : undefined; 
+    // note == 100 ? this.note100 = !this.note100 : undefined; 
+
+    function activate() {
+
+      document.querySelector('#note'+String(note))!.classList.add('btn-active')
+    }
+
+    function deactivate() {
+
+      document.querySelector('#note'+String(note))!.classList.remove('btn-active')
+    }
+
+
   }
 
 }
---
+++ src/app/components/operation/operation.model.ts
@@ -0,0 +1,7 @@
+export interface Operation {
+    name: string;
+    value: number;
+    note10?: boolean;
+    note50?: boolean;
+    note100?: boolean;
+}
