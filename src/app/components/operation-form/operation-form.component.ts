738bcd385357a9542a0f74d0a60099ebed0a9cfc
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-20 03:48:05 +0000

    feedback on operation creation

--- src/app/components/operation-form/operation-form.component.ts
+++ src/app/components/operation-form/operation-form.component.ts
@@ -1,56 +1,104 @@
-import { OperationService } from './../../operation.service';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { UIFeedbackService } from "./../../services/uifeedback.service"
+import { OperationService } from "./../../operation.service"
+import { FormGroup, FormControl, Validators } from "@angular/forms"
 import {
   Component,
   ElementRef,
+  EventEmitter,
   OnInit,
+  Output,
   QueryList,
   Renderer2,
+  ViewChild,
   ViewChildren,
-} from '@angular/core';
+} from "@angular/core"
+import {
+  fadeInOnEnterAnimation,
+  fadeOutOnLeaveAnimation,
+} from "angular-animations"
 @Component({
-  selector: 'app-operation-form',
-  templateUrl: './operation-form.component.html',
-  styles: ['div#packagesContainer::-webkit-scrollbar{display: none}'],
-  providers: [OperationService]
+  selector: "app-operation-form",
+  templateUrl: "./operation-form.component.html",
+  styles: ["div#packagesContainer::-webkit-scrollbar{display: none}"],
+  providers: [OperationService, UIFeedbackService],
+  animations: [fadeInOnEnterAnimation(), fadeOutOnLeaveAnimation()],
 })
 export class OperationFormComponent implements OnInit {
   form: FormGroup = new FormGroup({
-    name: new FormControl<string>('Operação 1', [Validators.required]),
+    name: new FormControl<string>("Operação 1", [Validators.required]),
     value: new FormControl<number | null>(null, [
       Validators.required,
       Validators.min(1000),
     ]),
     billType: new FormControl<number | null>(null, [Validators.required]),
-  });
+  })
 
-  @ViewChildren('bill10,bill50,bill100')
-  bills!: QueryList<ElementRef>;
+  @ViewChildren("bill10,bill50,bill100")
+  bills!: QueryList<ElementRef>
 
-  constructor(private renderer: Renderer2, private operationService: OperationService) {}
+  @ViewChild("submitButton") submitButton!: ElementRef
+
+  @Output() disableReturn = new EventEmitter<boolean>()
+
+  constructor(
+    private renderer: Renderer2,
+    private operationService: OperationService,
+    public ui: UIFeedbackService
+  ) {}
 
   ngOnInit(): void {}
 
-  toggleBill(billType: string): void {
-    let activeClass = '!bg-emerald-600';
+  toggleBill(billType: "10" | "50" | "100"): void {
+    let activeClass = "!bg-emerald-600"
 
     this.bills.forEach((el) => {
-      let bill = el.nativeElement as HTMLElement;
+      let bill = el.nativeElement as HTMLElement
       let isSelectedBill =
-        bill.getAttribute('id') == `bill${billType}` ? true : false;
+        bill.getAttribute("id") == `bill${billType}` ? true : false
 
       if (isSelectedBill) {
-        this.renderer.addClass(bill, activeClass);
+        this.renderer.addClass(bill, activeClass)
       } else {
-        this.renderer.removeClass(bill, activeClass);
+        this.renderer.removeClass(bill, activeClass)
       }
-    });
+    })
 
-    this.form.get('billType')?.setValue(Number(billType));
+    this.form.get("billType")?.setValue(Number(billType))
   }
 
   submitForm() {
+    this.setFeedback("loading")
+
     const operation = this.operationService.create(this.form.value)
-    console.log(operation.subscribe());
+
+    const response = {
+      complete: () => {
+        this.setFeedback("success")
+      },
+      error: (err: unknown) => {
+        this.setFeedback("error")
+        console.error("ew, error", err)
+      },
+    }
+
+
+    return operation.subscribe(response)
+  }
+
+  setFeedback(status: "loading" | "success" | "error") {
+    let btn = this.submitButton.nativeElement as HTMLElement
+    this.ui.feedback = status;
+
+    if (status === "loading") {
+      this.ui.buttonLoading.create(btn)
+      this.disableReturn.emit(true)
+      return
+    }
+
+    if (status === "success" || status === "error") {
+      this.ui.buttonLoading.dismiss(btn)
+      this.disableReturn.emit(false)
+    }
+    this.ui.timer(5, () => (this.ui.feedback = undefined))
   }
 }
--- src/app/components/operations-table/operations-table.component.ts
+++ src/app/components/operations-table/operations-table.component.ts
@@ -1,61 +1,81 @@
-import { OperationService } from "./../../operation.service";
-import { Component, OnInit } from "@angular/core";
-import { Operation } from "src/app/models/operation.model";
-import { Observable } from "rxjs";
+import { UIFeedbackService } from "./../../services/uifeedback.service"
+import { OperationService } from "./../../operation.service"
+import {
+  Component,
+  ElementRef,
+  OnChanges,
+  Renderer2,
+  ViewChild,
+} from "@angular/core"
+import { Operation } from "src/app/models/operation.model"
+import { Observable } from "rxjs"
 
 @Component({
-   selector: "app-operations-table",
-   templateUrl: "./operations-table.component.html",
-   styles: ["ul::-webkit-scrollbar{display: none;}"],
-   providers: [OperationService],
+  selector: "app-operations-table",
+  templateUrl: "./operations-table.component.html",
+  styles: ["ul::-webkit-scrollbar{display: none;}"],
+  providers: [OperationService],
 })
-export class OperationsTableComponent implements OnInit {
-   operations$: Observable<Operation[]> = this.operationService.findAll();
+export class OperationsTableComponent implements OnChanges {
+  operations$: Observable<Operation[]> = this.operationService.findAll()
 
-   showAddForm = false;
+  showAddForm = false
 
-   constructor(private operationService: OperationService) {}
+  @ViewChild("addOperationButton") button!: ElementRef
 
-   ngOnInit(): void {
-      console.log(this.operations$.subscribe());
-   }
+  constructor(
+    private operationService: OperationService,
+    private renderer: Renderer2,
+  ) {}
 
-   // it hides|shows all other app-operation that are not in focus.
-   toggleDetail(show: boolean, id: number) {
-      let displayProp = show ? "none" : "initial";
+  ngOnChanges(): void {}
 
-      let allOperations = document.querySelectorAll("app-operation");
+  setFeedback(loading: boolean) {
+    let btn = this.button.nativeElement as HTMLElement
 
-      allOperations.forEach((op) => {
-         // if the id is different from the parameter id, put display none|block
-         if (Number(op.getAttribute("id")) != id) {
-            let _op = op as HTMLElement;
-            _op.style.display = displayProp;
-         }
-      });
-   }
+    if (loading) {
+      this.renderer.setAttribute(btn, 'disabled', '')
+    } else {
+      this.renderer.removeAttribute(btn, 'disabled', '')
+    }
+  }
 
-   toggleAddOperationView() {
-      if (!this.showAddForm) {
-         document.getElementById("endDarkGradient")!.style.display = "none";
-         document.getElementById("operationsTable")!.style.display = "none";
+  // it hides|shows all other app-operation that are not in focus.
+  toggleDetail(show: boolean, id: number) {
+    let displayProp = show ? "none" : "initial"
 
-         document.getElementById(
-            "addOperationButton"
-         )!.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>`;
+    let allOperations = document.querySelectorAll("app-operation")
 
-         this.showAddForm = true;
-      } else {
-         document.getElementById("endDarkGradient")!.style.display = "initial";
-         document.getElementById("operationsTable")!.style.display = "initial";
-
-         document.getElementById(
-            "addOperationButton"
-         )!.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" /></svg>`;
-
-         this.showAddForm = false;
+    allOperations.forEach((op) => {
+      // if the id is different from the parameter id, put display none|block
+      if (Number(op.getAttribute("id")) != id) {
+        let _op = op as HTMLElement
+        _op.style.display = displayProp
       }
-   }
+    })
+  }
+
+  toggleAddOperationView() {
+    if (!this.showAddForm) {
+      document.getElementById("endDarkGradient")!.style.display = "none"
+      document.getElementById("operationsTable")!.style.display = "none"
+
+      document.getElementById(
+        "addOperationButton"
+      )!.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" /></svg>`
+
+      this.showAddForm = true
+    } else {
+      document.getElementById("endDarkGradient")!.style.display = "initial"
+      document.getElementById("operationsTable")!.style.display = "initial"
+
+      document.getElementById(
+        "addOperationButton"
+      )!.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M12 6v12m6-6H6" /></svg>`
+
+      this.showAddForm = false
+    }
+  }
 }
