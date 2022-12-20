c2b60755684481392eb90b7151eef4264b455e11
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-20 17:23:08 +0000

    improved operation view and corrected model

--- src/app/components/operation/operation.component.ts
+++ src/app/components/operation/operation.component.ts
@@ -1,34 +1,42 @@
-import { ApiService } from './../../services/api.service';
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { Operation } from 'src/app/models/operation.model';
+import { firstValueFrom } from "rxjs"
+import { OperationService } from "./../../operation.service"
+import { ApiService } from "./../../services/api.service"
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core"
+import { Operation } from "src/app/models/operation.model"
 
 @Component({
-  selector: 'app-operation',
-  templateUrl: './operation.component.html',
-  providers: [ApiService]
+  selector: "app-operation",
+  templateUrl: "./operation.component.html",
+  providers: [ApiService, OperationService],
 })
 export class OperationComponent implements OnInit {
+  @Input() operation!: Operation
 
-  @Input() operation!: Operation;
+  @Output() showDetailEvent = new EventEmitter<boolean>()
 
-  @Output() showDetailEvent = new EventEmitter<boolean>();
+  _showDetail = false
 
-  _showDetail = false;
+  constructor(
+    private api: ApiService,
+    private operationService: OperationService
+  ) {}
 
-  constructor(private api: ApiService) { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  async showDetail(operationId: number) {
+    this.showDetailEvent.emit(true)
+    this._showDetail = true
 
-  }
+    const source = this.operationService.findOne(operationId)
+    const operation = await firstValueFrom(source)
+    this.operation = operation;
 
-  showDetail() {
-    this.showDetailEvent.emit(true);
-    this._showDetail = true;
+    return operation
   }
 
   closeDetail() {
-    this.showDetailEvent.emit(false);
-    this._showDetail = false;
+    this.showDetailEvent.emit(false)
+    this._showDetail = false
   }
 
   delete(id: number) {
