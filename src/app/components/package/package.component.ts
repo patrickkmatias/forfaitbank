d583a182df77d2b52ef1926024948adce0f20a9d
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-21 19:05:54 +0000

    simplified package model and adjusted package component view to minify

--- src/app/components/package/package.component.ts
+++ src/app/components/package/package.component.ts
@@ -5,69 +5,79 @@ import {
   AfterViewInit,
   ViewChild,
   Renderer2,
-} from '@angular/core';
-import { Package } from 'src/app/models/package.model';
-import { ColorGeneratorService } from 'src/app/services/color-generator.service';
+} from "@angular/core"
+import { Package } from "src/app/models/package.model"
+import { ColorGeneratorService } from "src/app/services/color-generator.service"
 
 @Component({
-  selector: 'app-package',
+  selector: "app-package",
   providers: [ColorGeneratorService],
   template: `
     <div class="flex flex-col justify-center items-center">
       <div
         #h1wrapper
-        class="w-12 h-12 -mb-6 relative z-10 flex justify-center items-center text-3xl rounded-md shadow-lg"
+        [class.-mb-6]="!minify"
+        class="w-12 h-12 relative z-10 flex justify-center items-center text-3xl rounded-md shadow-lg"
       >
         <h1 #h1>🧳</h1>
       </div>
       <ul
         #ul
+        *ngIf="!minify"
         class="relative z-0 rounded-md pt-7 pb-1 px-2 text-center w-32 h-auto shadow-lg font-medium"
       >
         <li class="flex justify-between">
           <span>💵</span
           ><span class="flex items-center text-slate-900 text-sm">{{
-            pkg.billType | currency: 'BRL'
+            pkg.billType | currency : "BRL"
           }}</span>
         </li>
         <li class="flex justify-between">
           <span>🔢</span
-          ><span class="flex items-center text-slate-900 text-sm">{{ pkg.billQuantity }}</span>
+          ><span class="flex items-center text-slate-900 text-sm">{{
+            pkg.billQuantity
+          }}</span>
         </li>
         <li class="flex justify-between">
           <span class="pl-[0.1rem]">💰</span
-          ><span class="flex items-center text-slate-900 text-sm">{{
-            pkg.totalValue | currency: 'BRL'
-          }}</span>
+          ><span class="flex items-center text-slate-900 text-sm"
+            >{{
+            <!-- totalValue | currency: 'BRL' -->
+            }}</span
+          >
         </li>
       </ul>
     </div>
   `,
 })
 export class PackageComponent implements AfterViewInit {
-  @ViewChild('ul') ul!: ElementRef;
-  @ViewChild('h1') h1!: ElementRef;
-  @ViewChild('h1wrapper') h1wrapper!: ElementRef;
-  @Input('package') pkg!: Package;
+  @ViewChild("ul") ul!: ElementRef
+  @ViewChild("h1") h1!: ElementRef
+  @ViewChild("h1wrapper") h1wrapper!: ElementRef
+  @Input("package") pkg!: Package
+  @Input() minify: boolean = true
 
   constructor(private renderer: Renderer2, private cg: ColorGeneratorService) {}
 
   ngAfterViewInit(): void {
-    let palette = this.cg.generateColorPalette(this.pkg.color);
+    let palette = this.cg.generateColorPalette(this.pkg.color)
 
-    let filter = this.cg.correctIconColor(this.pkg.color);
+    let filter = this.cg.correctIconColor(this.pkg.color)
 
     // add random background color
-    this.renderer.setStyle(
-      this.ul.nativeElement,
-      'background-color',
-      palette[200]
-    );
+
+    if (!this.minify) {
+      this.renderer.setStyle(
+        this.ul.nativeElement,
+        "background-color",
+        palette[200]
+      )
+    }
     this.renderer.setStyle(
       this.h1wrapper.nativeElement,
-      'background-color',
+      "background-color",
       palette[500]
-    );
-    this.renderer.setStyle(this.h1.nativeElement, 'filter', filter);
+    )
+    this.renderer.setStyle(this.h1.nativeElement, "filter", filter)
   }
 }
--- src/app/models/package.model.ts
+++ src/app/models/package.model.ts
@@ -1,34 +1,7 @@
-import { ColorGeneratorService } from '../services/color-generator.service';
-import { Operation } from './operation.model';
-
-export class Package {
-  billType: number;
-  billQuantity: number;
-  status: string;
-  parentOperation: Operation;
-  totalValue: number;
-  color: string;
-
-  constructor(billType: number, billQuantity: number, parentOperation: Operation) {
-
-    if (billType == 10 || 50 || 100) {
-      this.billType = billType;
-    } else {
-      throw new Error('Invalid bill; valid bills are: 10, 50 and 100.');
-    }
-
-    const MAX_BILL_QUANTITY = 50;
-
-    if (billQuantity <= MAX_BILL_QUANTITY) {
-      this.billQuantity = billQuantity;
-    } else {
-      throw new Error('Package bill quantity cannot be over 50.');
-    }
-
-    let cg = new ColorGeneratorService();
-    this.totalValue = billType * billQuantity;
-    this.color = cg.generateRandomHexColor();
-    this.status = billQuantity == 50 ? 'closed' : 'opened';
-    this.parentOperation = parentOperation;
-  }
+export interface Package {
+  id: number
+  billType: 10 | 50 | 100
+  billQuantity: number
+  status: string
+  color: string
 }
