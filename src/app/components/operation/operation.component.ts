11b2f30e7abe5485efbb6eed79e41fedf2526ac3
Patrick M <patrickmatias.silva@hotmail.com>
2022-12-24 03:11:50 +0000

    package selection view implemented

--- src/app/components/operation/operation.component.ts
+++ src/app/components/operation/operation.component.ts
@@ -1,14 +1,9 @@
+import { Package } from "./../../models/package.model"
 import { UIFeedbackService } from "./../../services/uifeedback.service"
 import { firstValueFrom } from "rxjs"
 import { OperationService } from "./../../operation.service"
 import { ApiService } from "./../../services/api.service"
-import {
-  Component,
-  EventEmitter,
-  Input,
-  OnInit,
-  Output,
-} from "@angular/core"
+import { ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output } from "@angular/core"
 import { Operation } from "src/app/models/operation.model"
 import {
   fadeInOnEnterAnimation,
@@ -20,31 +15,47 @@ import {
   templateUrl: "./operation.component.html",
   providers: [OperationService, UIFeedbackService],
   animations: [fadeInOnEnterAnimation(), fadeOutOnLeaveAnimation()],
+  styles: [
+    `
+      ::-webkit-scrollbar {
+        display: none;
+      }
+      @media (min-width: 640px) {
+        ::-webkit-scrollbar {
+          display: block;
+          height: 0.7rem;
+        }
+      }
+    `,
+  ],
 })
 export class OperationComponent implements OnInit {
   constructor(
     private api: ApiService,
     private operationService: OperationService,
-    public ui: UIFeedbackService
-    ) {}
-  loadingDetail = false;
+    public ui: UIFeedbackService,
+    private cdr: ChangeDetectorRef
+  ) {}
   @Input() operation!: Operation
 
   @Output() showDetailEvent = new EventEmitter<boolean>()
-
   _showDetail = false
+  loadingDetail = false
+
+  selectedPackageId?: number
+  selectedPackage: Package | null = null;
 
   ngOnInit(): void {}
 
   async showDetail(operationId: number) {
     this.showDetailEvent.emit(true)
-    this.loadingDetail = true;
-    
+    this.loadingDetail = true
+
     const source = this.operationService.findOne(operationId)
     const operation = await firstValueFrom(source)
     this.operation = operation
 
-    this.loadingDetail = false;
+    this.loadingDetail = false
     this._showDetail = true
 
     console.log(operation)
@@ -76,5 +87,19 @@ export class OperationComponent implements OnInit {
     this.ui.feedback = status
     if (status === "error")
       this.ui.timer(5, () => (this.ui.feedback = undefined))
+  }
+
+  // its better by index
+  selectPackage(id: number) {
+    if (this.selectedPackage == null || this.selectedPackage.id !== id) {
+      const index = this.operation.packages.findIndex(
+        (pkg) => pkg.id === id
+      )!
+      this.selectedPackage = null
+
+      this.selectedPackage = this.operation.packages[index]
+    } else {
+      this.selectedPackage = null
+    }
   }
 }
